f1582c03d460de9ac5e85563fb8e4e11c23d3fba
rmesser87 <33704927+rmesser87@users.noreply.github.com>
2018-07-04 13:08:29 -0500

    standardized const user

--- models/mysql/user.js
+++ models/mysql/user.js
@@ -1,6 +1,6 @@
 module.exports = function (sequelize, Sequelize) {
     //do we want to define this as user of users
-    var User = sequelize.define('user', {
+    const User = sequelize.define('user', {
 
         id: {
             autoIncrement: true,
