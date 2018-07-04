a6c5f13aabea7763c520ac9f986f63ecdae92ffd
Antony Laroche <antony@larochefamily.net>
2018-07-04 12:26:09 -0500

    Added database routes and trading controller file

---
+++ controllers/tradingController.js
@@ -0,0 +1,23 @@
+const db = require('../models');
+
+module.exports = {
+
+    buy: function(req, res) {
+        db.Portfolio
+            .update({user_id: req.params.id}, {
+                $push:
+                    {trades: req.body},
+            })
+            .then((dbModel) => res.json(dbModel))
+            .catch((err) => res.status(422).json(err));
+    },
+    sell: function(req, res) {
+        db.Portfolio
+            .update({user_id: req.params.id}, {
+                $push:
+                    {trades: req.body},
+            })
+            .then((dbModel) => res.json(dbModel))
+            .catch((err) => res.status(422).json(err));
+    },
+};
