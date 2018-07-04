66f45e8246125c9d5f82a338c8f4b2661e967c66
Carolyn Vasisko <carolyn@mothermedia.com>
2018-07-03 23:33:47 -0500

    linted pages previously committed

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
-import Portfolio from "./pages/Portfolio";
-
-
+import Portfolio from './pages/Portfolio';
 
 const App = () => (
   <Router>
     <div>
-  
       <Switch>
-        
         <Route exact path="/portfolio" component={Portfolio} />
-        
       </Switch>
     </div>
   </Router>
--- client/src/components/PetStats/PetStats.js
+++ client/src/components/PetStats/PetStats.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Progress } from 'reactstrap';
+import {Progress} from 'reactstrap';
 
 /** This component will create a status bar for the user's pet.
  *  The value for each must be obtained from the DB, the var names revised.
  *  petStat = [Overall Health, Happiness, Fondness, Hunger]
  *  Styling TBD to match app styles
  */
- 
 
 const PetStats = (props) => {
   return (
     <div>
       <div className="text-left">{props.petStat}</div>
-      <Progress color={props.petStatColor} value={props.petStatValue} />      
+      <Progress color={props.petStatColor} value={props.petStatValue} />
     </div>
   );
 };
