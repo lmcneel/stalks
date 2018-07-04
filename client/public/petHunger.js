26b7eb174ad88d7b657205789d33d8dcd356f678
Brock Belmonte <brockbel@gmail.com>
2018-07-04 10:29:45 -0500

    Pseudocode for the hungerDrain and hungerFill Functions

---
+++ client/public/petHunger.js
@@ -0,0 +1,16 @@
+var hunger = 100;
+
+function hungerDrain() {
+
+// Drain hunger based on a time interval.
+// The drain will take effect upon login.
+// To calculate the amount of drain we will use the last time they logged in as a time stamp and subtract that from their current login then drain the hunger appropriately.
+
+}
+
+function hungerFill() {
+
+// Feeding your pet is how you will increase the hunger bar (meaning it will not need food for a while)
+// You can buy the food using in game currency in the shop, you can only own so much food at a time.
+
+}
