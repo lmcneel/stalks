4351155e13d7ec18f0b7a60a2ec70487821c3108
Carolyn Vasisko <carolyn@mothermedia.com>
2018-07-06 10:21:20 -0500

    added name, image, and wrapper to petstats...also added test page component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Portfolio from './pages/Portfolio';
+import Test from './pages/Test';
 
 const App = () => (
   <Router>
     <div>
       <Switch>
         <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/test" component={Test} />
       </Switch>
     </div>
   </Router>
