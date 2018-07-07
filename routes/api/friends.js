e1debf4546afd1b5cd635a2ffb5bb17b0d59e20f
Alex Szoeke <alex_szoeke@outlook.com>
2018-07-07 13:35:59 -0500

    start of friends api

---
+++ routes/api/friends.js
@@ -0,0 +1,22 @@
+//Server side
+const router = require('express').Router();
+const request = require('request');
+
+//Models
+//will need user table
+//will need friends table
+
+
+router.get('/api/view/friends', (req, res) => {
+
+});
+
+router.post('/api/create/friend', (req, res) => {
+
+});
+
+router.delete('/api/delete/friend ', (req, res) => {
+
+});
+
+
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const tradingRoutes = require('./trading');
+const friendRoutes = require('./friends');
 
 // Trading routes
 router.use('/trading', tradingRoutes);
+router.use('/friends', friendRoutes);
 
 module.exports = router;
