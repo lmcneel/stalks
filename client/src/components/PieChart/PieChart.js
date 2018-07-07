bdfd16b37d810419a4b371796f7e439e42ab9d91
Carolyn Vasisko <carolyn@mothermedia.com>
2018-07-06 21:55:09 -0500

    linted pages

--- client/src/components/PieChart/PieChart.js
+++ client/src/components/PieChart/PieChart.js
@@ -8,13 +8,13 @@ const PieChart = (props) => {
     return (
       <div>
       <VictoryPie
-         colorScale={["tomato", "orange", "gold", "cyan", "navy" ]}
+         colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy']}
          height={250}
          data={[
-        { x: "Company A", y: 35 },
-        { x: "Company B", y: 40 },
-        { x: "Company C", y: 55 },
-        { x: "Company D", y: 75 },
+        {x: 'Company A', y: 35},
+        {x: 'Company B', y: 40},
+        {x: 'Company C', y: 55},
+        {x: 'Company D', y: 75},
         ]}
       />
       </div>
