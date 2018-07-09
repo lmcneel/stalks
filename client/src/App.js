d72121582c01dcb5bd3557857869dae01fa2624a
christopher demeke <christopher.demeke@gmail.com>
2018-07-08 22:10:33 -0500

    removed import App.css from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import './App.css';
 import Trading from './pages/Trading';
 
 const App = () => (
