9bc68809f5b8d47830aa842d9abf48097f8a7681
grantposell <grantposell@gmail.com>
2018-07-09 18:42:14 -0500

    Added Post Forum API

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const tradingRoutes = require('./trading');
+const forumRoutes = require('./forum');
 
 // Trading routes
 router.use('/trading', tradingRoutes);
+router.use('/forum', forumRoutes);
 
 module.exports = router;
