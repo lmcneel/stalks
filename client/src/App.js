370c8eee2b8cf8de0cbbf99c7b697f390eb44772
Brock Belmonte <brockbel@gmail.com>
2018-07-09 20:31:58 -0500

    Fixed issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Inventory from './components/userFoodAndToys';
 // import CollapseRow from './components/CollapseRow/index';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Petfolio from './pages/Petfolio';
 import Trading from './pages/Trading';
-=======
 import Inventory from './components/userTabs';
->>>>>>> e19ba6a25b0673bed040a406b54277ae070b00a4
 
 /**
  * Class App
@@ -21,6 +18,7 @@ class App extends Component {
   render() {
     return (
       <div>
+      <div>
         <Inventory />
       </div>
 
@@ -32,6 +30,7 @@ class App extends Component {
           </Switch>
         </div>
       </Router>
+      </div>
     );
   }
 }
