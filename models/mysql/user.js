c019240083d72fc7ad48a3cad3bc9562d6a3f0f2
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-07-11 14:18:00 -0500

    I don't see any changes but trying to move forward

--- models/mysql/user.js
+++ models/mysql/user.js
@@ -1,93 +1,24 @@
-module.exports = function(sequelize, Sequelize) {
-    // do we want to define this as user of users
+const bcrypt = require('bcryptjs');
+module.exports = function(sequelize, DataTypes) {
+// User Schema
     const User = sequelize.define('User', {
-
-        id: {
-            autoIncrement: true,
-            primaryKey: true,
-            type: Sequelize.INTEGER,
-        },
-        // we can combined the to just name if user auth wants this
-        firstname: {
-            type: Sequelize.STRING,
-            notEmpty: true,
-        },
-        lastname: {
-            type: Sequelize.STRING,
-            notEmpty: true,
-        },
-        // fullname: {
-        //     type: Sequelize.STRING,
-        //     notEmpty: true
-        // },
         username: {
-            type: Sequelize.STRING,
-            notEmpty: true,
-        },
-        email: {
-            type: Sequelize.STRING,
-            validate: {
-                isEmail: true,
-            },
+            type: DataTypes.STRING,
         },
         password: {
-            type: Sequelize.STRING,
-            allowNull: false,
+            type: DataTypes.STRING,
         },
-        // haven't seen levels incorporated in the current scope but if necessary
-        // level: {
-        //     type: Sequelize.INTEGER
-        // },
-        // should be created by association
-        // title_id: {
-        //     type: Sequelize.INTEGER,
-        //     allowNull: false
-        // },
-        balance: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
+        email: {
+            type: DataTypes.STRING,
         },
-        mongo_id: {
-            type: Sequelize.STRING,
-            allowNull: false,
+        name: {
+            type: DataTypes.STRING,
         },
-        last_login: {
-            type: Sequelize.DATE,
-            allowNull: true,
-        },
-        //  this would give us the ability to make accounts inactive is this is decided upon
-        status: {
-            type: Sequelize.ENUM('active', 'inactive'),
-            defaultValue: 'active',
-        },
-    }, {
-        underscored: true,
+
     });
-
-    // names of other models have not been established so the associations are subject to change
-    User.associate = function(models) {
-        // at this point we are assuming users only have one pet
-        User.hasOne(models.Pet, {
-            onDelete: 'cascade',
-        }),
-        // haven't seen title incorporated in the current scope but if necessary
-        // User.belongsTo(models.titles),
-        User.hasMany(models.UserWatchlist, {
-            onDelete: 'cascade',
-        }),
-        User.hasMany(models.UserLogins, {
-            onDelete: 'cascade',
-        }),
-        User.hasMany(models.Friends, {
-            onDelete: 'cascade',
-        });
-        // User.hasMany(models.users_accomplishments, {
-        //     onDelete: 'cascade'
-        // }),
-        // User.hasMany(models.users_gifts, {
-        //     onDelete: 'cascade'
-        // });
+    // A custom method for our User model. It will compare user pawword wit stored password.
+    User.prototype.validPassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
     };
-
     return User;
 };
