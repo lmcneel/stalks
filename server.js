c2f961b165a97f3767ef8449d0418b86b96c4a84
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-07-11 12:31:59 -0500

    adding some files for acheivements and put a small change on server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(bodyParser.json());
 
 // Define middleware here
 app.use(express.json());
+app.use(acheivements);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
