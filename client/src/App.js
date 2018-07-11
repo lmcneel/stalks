b89566c6dfe06b7648a7fcf34044c6e728c2ca6f
nomamanomama <33500937+nomamanomama@users.noreply.github.com>
2018-07-10 22:45:42 -0500

    added logout form, api util and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import CollapseRow from './components/CollapseRow/index';
 import SigninForm from './components/SigninForm';
@@ -7,14 +6,11 @@ import SignupForm from './components/SignupForm';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {faFacebookF, faGoogle} from '@fortawesome/fontawesome-free-brands';
 import {faAt, faKey} from '@fortawesome/fontawesome-free-solid';
-
-library.add(faFacebookF|faGoogle|faAt|faKey);
-=======
-// import CollapseRow from './components/CollapseRow/index';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Petfolio from './pages/Petfolio';
 import Trading from './pages/Trading';
->>>>>>> dev
+library.add(faFacebookF|faGoogle|faAt|faKey);
+
+
 
 /**
  * Class App
@@ -26,7 +22,6 @@ class App extends Component {
    */
   render() {
     return (
-<<<<<<< HEAD
       <Router>
       <div className='App'>
         <header className='App-header'>
@@ -34,21 +29,13 @@ class App extends Component {
         </header>
           <Switch>
             <Route exact path='/login' component={SigninForm} />
+            <Route exact path='/logout' component={SignoutForm} />
             <Route exact path='/signup' component={SignupForm} />
-            
-          </Switch>
-        <CollapseRow />
-      </div>
-=======
-
-      <Router>
-        <div>
-          <Switch>
             <Route exact path="/petfolio" component={Petfolio} />
             <Route exact path='/trading' component={Trading} />
           </Switch>
-        </div>
->>>>>>> dev
+        <CollapseRow />
+      </div>
       </Router>
     );
   };
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,24 +5,17 @@ import axios from 'axios';
 export default {
 
     buyShares: function(buyData) {
-        return axios.post(`/api/trading/buy/`, buyData);
+        return axios.post('/api/trading/buy', buyData);
     },
     sellShares: function(sellData) {
-        return axios.post(`/api/trading/sell/`, sellData);
+        return axios.post('/api/trading/sell', sellData);
     },
-    findQuotes: function(data) {
-        console.log(data);
-        return axios.get(`/api/trading/quote/${data.ticker}`);
-    },
-    getPortfolioValue: function() {
-        return axios.get('api/petfolio/portfolio');
-    },
-    getBankValue: function() {
-        return axios.get('api/petfolio/bank');
+    findQuotes: function(symbol) {
+        return axios.get('/api/trading/quote', symbol);
     },
 
-    // Saves an article to the database
-    login: function (loginData) {
+    // login axios call
+    login: function(loginData) {
         return axios.post('/api/login', loginData)
             .then((response) => {
                 console.log(response);
@@ -31,9 +24,19 @@ export default {
                 console.log(error);
             });
     },
-    // Saves an article to the database
-    signup: function (signupData) {
+    // signup axios call
+    signup: function(signupData) {
         return axios.post('/api/signup', signupData)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    // Saves an article to the database
+    logout: function (logoutData) {
+        return axios.post('/api/logout', logoutData)
             .then((response) => {
                 console.log(response);
             })
