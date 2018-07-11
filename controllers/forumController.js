df2e77d167fd5c0fb2288a9d681204bd07a906de
grantposell <grantposell@gmail.com>
2018-07-10 20:08:39 -0500

    Pulled social into branch

--- controllers/forumController.js
+++ controllers/forumController.js
@@ -1,1 +1,6 @@
-//
+//Require the models from mongo
+//Select a topic (General, Stocks, Help)
+//Write in subject into line
+//create subject via hitting submit
+//create comment by writing into the space, click submit to enter a comment
+//post comment to board
