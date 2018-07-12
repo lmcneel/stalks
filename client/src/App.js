8b750fdcf7e7c38bed54ee3f99d41f41b687e978
allisa liberto <allisa84@yahoo.com>
2018-07-11 19:55:11 -0500

    switch router created

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,16 @@ import Content from './components/Content';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Petfolio from './pages/Petfolio';
 import Trading from './pages/Trading';
+// import PetCenter from './components/pages/PetCenter';
+// import Forum from './components/pages/Forum';
+// import Friends from './components/pages/Friends';
+import Home from './pages/Home';
+import About from './pages/About';
+// import Settings from './components/pages/Settings';
+// import Login from './components/pages/Login';
+// import Logout from './components/pages/Logout';
+// import SignUp from './components/pages/SignUp';
+// import Achievements from './components/pages/Achievements';
 
 /**
  * Class App
@@ -47,10 +57,20 @@ class App extends Component {
                 <PortfolioStatus />
                 <WatchlistTicker />
                 <Content>
-                    <Switch>
+                <Switch>
                       <Route exact path="/petfolio" component={Petfolio} />
                       <Route exact path='/trading' component={Trading} />
-                    </Switch>
+                      {/* <Route exact path='/petcenter' component={PetCenter} />
+                      <Route exact path='/friends' component={Friends} />
+                      <Route exact path='/forum' component={Forum} /> */}
+                      <Route exact path='/' component={Home} />
+                      <Route exact path='/about' component={About} />
+                      {/* <Route exact path='/settings' component={Settings} />
+                      <Route exact path='/login' component={Login} />
+                      <Route exact path='/logout' component={Logout} />
+                      <Route exact path='/signup' component={SignUp} />
+                      <Route exact path='/achievements' component={Achievements} /> */}
+                     </Switch>
                 </Content>
                 </MainContentWrapper>
               </Wrapper>
