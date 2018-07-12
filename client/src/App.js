cde5aab41355027398394a53cecfabc2938288e5
grantposell <grantposell@gmail.com>
2018-07-11 20:43:00 -0500

    Fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import Content from './components/Content';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Petfolio from './pages/Petfolio';
 import Trading from './pages/Trading';
-
 import ViewStocks from './pages/ViewStocks';
 import DocsList from './components/DocsList/DocsList';
 import HelpLanding from './components/HelpLanding/HelpLanding';
@@ -18,65 +17,58 @@ import Social from './pages/Social/Social.js';
 
 
 /**
- * Class App
- */
+* Class App
+*/
 class App extends Component {
-  /**
-   * @param {*} props
-   */
-  constructor(props) {
-    super(props);
-    this.state = {
-      sideNav: false,
-    };
-    this.navToggleHandler = this.navToggleHandler.bind(this);
-  }
+ /**
+  *@param {*} props
+  */
+ constructor(props) {
+   super(props);
+   this.state = {
+     sideNav: false,
+   };
+   this.navToggleHandler = this.navToggleHandler.bind(this);
+ }
 
-  /**
-   * Function that handles the click for the nav button
-   * @param {*} e
-   */
-  navToggleHandler(e) {
-    this.setState({sideNav: !this.state.sideNav});
-  }
-  /**
-   * Render function for App Component
-   * @return {JSX}
-   */
-  render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/petfolio" component={Petfolio} />
-            <Route exact path='/trading' component={Trading} />
-            <Route exact path= "/social" component={Social} />
-           </Switch>
-
-        <div className="App">
-            <TopNav navToggleHandler={this.navToggleHandler}/>
-            <Wrapper>
-              <SideNav isActive={this.state.sideNav}/>
-              <MainContentWrapper>
-                <PortfolioStatus />
-                <WatchlistTicker />
-                <Inventory />
-                <Content>
-                    <Switch>
-                      <Route exact path="/petfolio" component={Petfolio} />
-                      <Route exact path='/trading' component={Trading} />
-                      <Route exact path= "/social" component={Social} />
-                      <Route exact path='/viewstocks' component={ViewStocks} />
-                      <Route exact path='/help' component={HelpLanding} />
-                      <Route exact path='/docs' component={DocsList} />
-                    </Switch>
-                </Content>
-                </MainContentWrapper>
-              </Wrapper>
-        </div>
-      </Router>
-    );
-  }
+ /**
+  * Function that handles the click for the nav button
+  *@param {*} e
+  */
+ navToggleHandler(e) {
+   this.setState({sideNav: !this.state.sideNav});
+ }
+ /**
+  * Render function for App Component
+  * @return {JSX}
+  */
+ render() {
+   return (
+     <Router>
+       <div className="App">
+           <TopNav navToggleHandler={this.navToggleHandler}/>
+           <Wrapper>
+             <SideNav isActive={this.state.sideNav}/>
+             <MainContentWrapper>
+               <PortfolioStatus />
+               <WatchlistTicker />
+               <Inventory />
+               <Content>
+                   <Switch>
+                     <Route exact path="/petfolio" component={Petfolio} />
+                     <Route exact path='/trading' component={Trading} />
+                     <Route exact path= "/social" component={Social} />
+                     <Route exact path='/viewstocks' component={ViewStocks} />
+                     <Route exact path='/help' component={HelpLanding} />
+                     <Route exact path='/docs' component={DocsList} />
+                   </Switch>
+               </Content>
+               </MainContentWrapper>
+             </Wrapper>
+       </div>
+     </Router>
+   );
+ }
 }
 
 export default App;
