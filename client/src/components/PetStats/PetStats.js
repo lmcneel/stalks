7b88a6edcef23b756d38be57911d2d762a084c5f
Brock Belmonte <brockbel@gmail.com>
2018-07-12 19:27:39 -0500

    Merging pets into issue#48

--- client/src/components/PetStats/PetStats.js
+++ client/src/components/PetStats/PetStats.js
@@ -8,6 +8,31 @@ import {Progress} from 'reactstrap';
  *  petStatValue = % filled
  */
 
+ // ============================================================
+
+let hunger;
+
+const hungerDrain = () => {
+
+// Drain hunger based on a time interval.
+// The drain will take effect upon login.
+// To calculate the amount of drain we will use the last time they logged in as a time stamp and subtract that from their current login then drain the hunger appropriately.
+// 24 hours until your pet food reaches 0
+// (Last login timestamp) - (Current login timestamp) = (Hunger drain)
+
+
+}
+
+const hungerFill = () => {
+
+// Feeding your pet is how you will increase the hunger bar (meaning it will not need food for a while)
+// You can buy the food using in game currency in the shop, you can only own so much food at a time.
+
+
+}
+
+ // ============================================================
+
 export const PetStats = (props) => {
   return (
     <div>
