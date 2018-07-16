c55a0d2fca1e5506dfe8ad1754750cc7bc007b1c
rmesser87 <33704927+rmesser87@users.noreply.github.com>
2018-07-16 00:06:17 -0500

    db queries for name and picture

--- client/src/components/PetStats/PetPic.js
+++ client/src/components/PetStats/PetPic.js
@@ -3,6 +3,18 @@ import wolfy from '../../assets/images/Wolf1.svg';
 import bear from '../../assets/images/Bear1.svg';
 import bull from '../../assets/images/Bull1.svg';
 import ostrich from '../../assets/images/Ostrich1.svg';
+import ostrichrc from '../../assets/images/OstrichRC1.svg';
+import ostrichbc from '../../assets/images/OstrichBC1.svg';
+import ostrichbt from '../../assets/images/OstrichBowtie1.svg';
+import bullrc from '../../assets/images/BullRC1.svg';
+import bullbc from '../../assets/images/BullBC1.svg';
+import bullbt from '../../assets/images/BullBowtie1.svg';
+import bearrc from '../../assets/images/BearRC1.svg';
+import bearbc from '../../assets/images/BearBC1.svg';
+import bearbt from '../../assets/images/BearBowtie1.svg';
+import wolfyrc from '../../assets/images/WolfRC1.svg';
+import wolfybc from '../../assets/images/WolfBC1.svg';
+import wolfybt from '../../assets/images/WolfBowtie1.svg';
 import API from '../../utils/API';
 // import other images here
 
@@ -27,14 +39,42 @@ class PetPic extends Component {
         API.getUserPic().then(((r) => {
             console.log(r.data);
             let url = r.data.urlImage;
-            if (url = './client/src/assets/images/Ostrich1.svg') {
-                this.setState({petPic: ostrich});
+            console.log(r.data.Accessories[0].equipped);
+            let accessory = r.data.Accessories[0].equipped;
+            let type = r.data.Accessories[0].name;
+            console.log(type);
+            if (url = './client/src/assets/images/Ostrich1.svg' && accessory == true && type === 'red collar') {
+                this.setState({petPic: ostrichrc});
+            } else if (url = './client/src/assets/images/Ostrich1.svg' && accessory == true && type === 'blue collar') {
+                this.setState({petPic: ostrichbc});
+            } else if (url = './client/src/assets/images/Ostrich1.svg' && accessory == true && type === 'bow tie') {
+                this.setState({petPic: ostrichbt});
+            } else if (url = './client/src/assets/images/Bull1.svg' && accessory == true && type === 'red collar') {
+                this.setState({petPic: bullrc});
+            } else if (url = './client/src/assets/images/Bull1.svg' && accessory == true && type === 'blue collar') {
+                this.setState({petPic: bullbc});
+            } else if (url = './client/src/assets/images/Bull1.svg' && accessory == true && type === 'bow tie') {
+                this.setState({petPic: bullbt});
+            } else if (url = './client/src/assets/images/Bear1.svg' && accessory == true && type === 'red collar') {
+                this.setState({petPic: bearrc});
+            } else if (url = './client/src/assets/images/Bear1.svg' && accessory == true && type === 'blue collar') {
+                this.setState({petPic: bearbc});
+            } else if (url = './client/src/assets/images/Bear1.svg' && accessory == true && type === 'bow tie') {
+                this.setState({petPic: bearbt});
+            } else if (url = './client/src/assets/images/Wolf1.svg' && accessory == true && type === 'red collar') {
+                this.setState({petPic: wolfyrc});
+            } else if (url = './client/src/assets/images/Wolf1.svg' && accessory == true && type === 'blue collar') {
+                this.setState({petPic: wolfybc});
+            } else if (url = './client/src/assets/images/Wolf1.svg' && accessory == true && type === 'bow tie') {
+                this.setState({petPic: wolfybt});
             } else if (url = './client/src/assets/images/Bull1.svg') {
                 this.setState({petPic: bull});
             } else if (url = './client/src/assets/images/Bear1.svg') {
                 this.setState({petPic: bear});
-            } else {
+            } else if (url = './client/src/assets/images/Wolf1.svg') {
                 this.setState({petPic: wolfy});
+            } else {
+                this.setState({petPic: ostrich});
             };
               console.log(r.data.urlImage);
           }));
