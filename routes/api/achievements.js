2471d1c16d1dc73de5cfa140a87afa918b9209e9
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-07-17 11:38:55 -0500

    adding achievements api

--- routes/api/achievements.js
+++ routes/api/achievements.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-// This is a test to see if the user is logging in and we are able to grab the login and add to achievements
-const userLogin1 = function(req, res, next) {
-  req.userLogin1 = 1;
-  next();
-};
-
-// This is test to see if the user has logged in 5 days in a row
-const userLoginTest = function(req, res, next) {
-console.log('This is just a test for logins');
-next();
-};
-
-module.exports = userLoginTest;
-=======
 const router = require('express').Router();
 // const User = require('../../models/mysql/user')
 router.route('/achievements')
@@ -23,4 +8,3 @@ router.route('/achievements')
 });
 
 module.exports = router;
->>>>>>> c4673eb9f900bb07734dfea82a102e5ec2caed38
