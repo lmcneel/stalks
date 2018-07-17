ecbdd4e06f118f1a67ad00f47e4f3ad46fc439f5
Michael Tran <tranmike196@gmail.com>
2018-07-17 10:39:14 -0500

    Sign up sign out login all correctly redirects. Added hook to create portfolio in mongo for user

--- client/src/components/Splash/Splash.js
+++ client/src/components/Splash/Splash.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Container, Row, Col, Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import PropTypes from 'prop-types';
+import SignUp from '../../pages/SignUp';
+import Login from '../../pages/Login';
 
+/**
+ * Function the Returns Profile Bar on left with all user info
+ * @param { object } props Object with Props Passed.
+ * @return { Splash }
+ */
+class Splash extends Component {
+     /**
+     * Constructor function for setting state
+     * @param {*} props
+     */
+    constructor(props){
+        super(props);
+        this.state = {
+            showSignIn: true,
+        };
 
-const Splash = (props) => <Container>
+        this.toggleSignup = this.toggleSignup.bind(this);
+    };
+
+    componentDidMount() {
+
+        this.props.backgroundColor
+    };
+    toggleSignup() {
+        console.log('heelo')
+        this.setState({showSignIn: !this.state.showSignIn});
+    }
+    render() {
+        return(
+<Container className=''>
     <Row className="text-center mt-5">
         <Col>
         <img src="http://placekitten.com/700/300" className="mx-auto d-block"/>
@@ -12,28 +43,30 @@ const Splash = (props) => <Container>
     </Row>
     <Row>
         <Col className="bg-light">
-            <Form className="">
-                <FormGroup>
-                <Label for="username">Username</Label>
-                <Input type="username" name="username" id="userName" />
-                </FormGroup>
-                <FormGroup>
-                <Label for="password">Password</Label>
-                <Input type="password" name="password" id="userPassword" />
-                </FormGroup>
-                <Button>Log-in</Button>
-            </Form>
-            </Col>
+            {this.state.showSignIn ? (
+                <div>
+                <Login />
+                <Button onClick={this.toggleSignup}> Sign Up </Button>
+                </div>
+            ) : (
+                <div>
+                <SignUp />
+                <Button onClick={this.toggleSignup}> Sign In </Button>
+                </div>
+            )}
+        </Col>
     </Row>
 
-    <Row>
+    {/* <Row>
         <Col>
         <p>Get a Stock Market Pet Today</p>
         <Button>Sign up</Button>
         </Col>
-    </Row>
+    </Row> */}
     </Container>
+        )
+    }
+}
 
-;
 
 export default Splash;
--- models/mongo/portfolio.js
+++ models/mongo/portfolio.js
@@ -7,22 +7,24 @@ const Schema = mongoose.Schema;
 // Using the Schema constructor, create a new UserSchema object
 const portfolioSchema = new Schema({
 
-
-  // user_id: {
-  //   type: mongoose.SchemaTypes.ObjectId,
-  //   ref: 'User'
-  // },
+  // Good to include mongo since we're using 2 databases if we ever need to update
+  // through user modal we know where to start
+  MongoUser_id: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'User',
+    required: true,
+  },
   // `cash` is required and of type Number, will be updated after each buy/sell transaction
   cash: {
     type: Number,
     required: true,
+    default: 300000,
   }, // db.portfolio.update({user_id: "XXXXXX"}, {$set: {cash:3000000}})
 
   // `currentValue` is type Number, this is the most recent calculation of portfolio value
   currentValue: {
     type: Number,
   }, // db.portfolio.update({user_id: "XXXXXX"}, {$set: {currentValue:150000000}})
-
   // `trades` is an array each time a new transaction is completed it will be pushed to this array
   trades: [{
     type: mongoose.Schema.Types.ObjectId,
